54787c2cf1f6d14af60a1a63ce97031e9b7882df
MelonMusk64 <j.krzysztof13@gmail.com>
2019-11-11 00:05:46 +0100

    url on tap

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -10,6 +11,9 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
+
+var _index;
+var _data;
 
 class _MyAppState extends State<MyApp> {
   List _data;
@@ -39,40 +43,20 @@ class _MyAppState extends State<MyApp> {
         ),
         body: new ListView.builder(
             itemCount: _data == null ? 0 : _data.length,
-            itemBuilder: (BuildContext context, int index) {
+            itemBuilder: (BuildContext context, _index) {
               return Column(
                 children: <Widget>[
                   Card(
                     child: ListTile(
-                      leading: Image.network(_data[index]["avatar"]),
-                      title: Text(_data[index]["name"]),
-                      subtitle: Text(_data[index]["description"]),
+                      leading: Image.network(_data[_index]["avatar"]),
+                      title: Text(_data[_index]["name"]),
+                      subtitle: Text(_data[_index]["description"]),
                       isThreeLine: true,
+                      onTap: () => launch(_data[_index]["url"])
                     ),
                   ),
                 ],
               );
-              /*Card(
-                child: Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.all(10),
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        data[index]["name"],
-                        style: DefaultTextStyle.of(context)
-                            .style
-                            .apply(fontSizeFactor: 1.8),
-                        textAlign: TextAlign.left,
-                      ),
-                      Text(
-                        data[index]["description"],
-                        textAlign: TextAlign.left,
-                      )
-                    ],
-                  ),
-                ),
-              );*/
             }),
       ),
     );
