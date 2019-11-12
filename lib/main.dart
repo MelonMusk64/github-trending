7f7c1f73f82dcb10047c5a4a38157e91be8cc8da
MelonMusk64 <j.krzysztof13@gmail.com>
2019-11-12 23:11:50 +0100

    futurebuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
+import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:url_launcher/url_launcher.dart';
 
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+class ReposData {
+  final String author;
+  final String name;
+  final String avatar;
+  final String url;
+  final String description;
+  final int stars;
+  final int forks;
+
+  ReposData(this.author, this.name, this.avatar, this.url, this.description,
+      this.stars, this.forks);
+}
 
 void main() => runApp(MyApp());
 
@@ -12,26 +24,22 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-var _index;
-var _data;
-
 class _MyAppState extends State<MyApp> {
-  List _data;
+  Future<ReposData> reposData;
 
-  Future<String> getData() async {
-    var response = await http.get(
+  List<ReposData> reposdetails = [];
+  Future<List<ReposData>> reposDetails() async {
+    var data = await http.get(
         "https://github-trending-api.now.sh/repositories?language=&since=daily");
+    var jsonData = json.decode(data.body);
 
-    this.setState(() {
-      _data = json.decode(response.body);
-    });
+    for (var rval in jsonData) {
+      ReposData books = ReposData(rval['author'], rval['name'], rval['avatar'],
+          rval['url'], rval['description'], rval['stars'], rval['forks']);
+      reposdetails.add(books);
+    }
+    return reposdetails;
   }
-
-  @override
-  void initState() {
-    this.getData();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -41,23 +49,36 @@ class _MyAppState extends State<MyApp> {
           title: Text("GitHub Trending"),
           backgroundColor: Color(0xFFFF7C00),
         ),
-        body: new ListView.builder(
-            itemCount: _data == null ? 0 : _data.length,
-            itemBuilder: (BuildContext context, _index) {
-              return Column(
-                children: <Widget>[
-                  Card(
-                    child: ListTile(
-                      leading: Image.network(_data[_index]["avatar"]),
-                      title: Text(_data[_index]["name"]),
-                      subtitle: Text(_data[_index]["description"]),
-                      isThreeLine: true,
-                      onTap: () => launch(_data[_index]["url"])
-                    ),
-                  ),
-                ],
-              );
-            }),
+        body: Center(
+          child: FutureBuilder(
+            future: reposDetails(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Column(
+                      children: <Widget>[
+                        Card(
+                          child: ListTile(
+                            leading: Image.network(snapshot.data[index].avatar),
+                            title: Text(snapshot.data[index].name),
+                            subtitle: Text(snapshot.data[index].description),
+                            isThreeLine: true,
+                            onTap: () => launch(snapshot.data[index].url),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
       ),
     );
   }
